e90662d96b25af922c4b1a8aa3035bd0eaa956c1
Mayank Bagari <68625003+mynkbzz@users.noreply.github.com>
2022-09-29 23:34:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ import jinja2
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 import asyncpg
-import sqlalchemy
-import databases
-
-
-# DATABASE_URL = "postgresql://postgres:root@localhost/testdb"
-
-# database = databases.Database(DATABASE_URL)
 
 
 app = FastAPI()
@@ -42,12 +35,18 @@ templates = Jinja2Templates(directory="templates")
 
 async def getlink():
     sql = f"Select link FROM randomlinks ORDER BY RANDOM() LIMIT 1"
-    links_is = await conn.fetch(sql)
-    return links_is
+    links_is = await conn.fetchrow(sql)
+    return str(links_is)
 
 
 @app.get("/front/", response_class=HTMLResponse)
 async def read_item(request: Request):
     linklelo = await getlink()
-    context = {"request": request, "linklelo": linklelo}
+    videolink = linklelo
+    context = {
+        "request": request,
+        "linklelo": linklelo,
+        "videourl": linklelo,
+        "videolink": videolink,
+    }
     return templates.TemplateResponse("front.html", context)
